39d67012516c13fd65febb6d980f1a4adaee15a3
HectorGuzman <hectorguzmancortes@gmail.com>
2025-04-17 22:01:39 -0400

    PWA

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'supstatus-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/data.json',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
